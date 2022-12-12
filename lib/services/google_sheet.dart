069d9314ac6766b6fbdf4b0a217004f39398468c
Paul Imoke <pauleke65@gmail.com>
2022-12-12 23:18:36 +0000

    update 2

--- lib/services/google_sheet.dart
+++ lib/services/google_sheet.dart
@@ -3,7 +3,18 @@ import 'package:movie_app/models/movie.dart';
 
 class SheetsAPI {
   static const _credentials = r'''
-
+{
+  "type": "service_account",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": ""
+}
 ''';
 
   static const _spreadsheetId = '';
